981be0b968fb09918777784e32210cbe6aa0797f
satyamwathrey <swathrey1@gmail.com>
2022-04-13 23:14:28 +0530

    Ability to create Queue of 56->30->70

---
+++ LinkedListQueue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class LinkedListQueue
+    {
+        Node head = null;
+
+        //Enqueue Function
+        internal void Enqueue(int data)
+        {
+            //Node Object
+            Node node = new Node(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            //Display Added Data
+            Console.WriteLine("{0} inserted into Queue ", node.data);
+        }
+        //Display Queue Data
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine("Queue Data : " + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,22 +14,31 @@ namespace StacksAndQueues
             Console.WriteLine("=-=-=-=-=-Welcome To The Stacks And Queues Program-=-=-=-=-=");
             Console.WriteLine("----------------------------");
 
-            //Calling Class and Adding Data in Stack
+            /*//Calling Class and Adding Data in Stack
             LinkedListStack linkedListStack = new LinkedListStack();
             linkedListStack.Push(70);
             linkedListStack.Push(30);
             linkedListStack.Push(56);
             Console.WriteLine("----------------------------");
-
+            
             //Peek Value from Stack
             linkedListStack.Peek();
             Console.WriteLine("----------------------------");
             linkedListStack.Pop();
-
             //Pop value till Stack will empty
             linkedListStack.IsEmpty();
+*/
+            //Calling Class and Adding Data in Stack
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            Console.WriteLine("----------------");
 
-            Console.ReadLine(); ;
+            //Display Queue
+            linkedListQueue.Display();
+
+            Console.ReadLine();
         }
     }
 }
